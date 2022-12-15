686788fc8483ce8f9602478dfb20a1bf4afd0736
Ngo Thanh Long <ngothanhlong19943@gmail.com>
2022-12-15 08:15:54 +0700

    For NewsAPI Page, still fetch and show the list, view details not ready

---
+++ app/src/main/java/com/notmiyouji/newsapp/NewsAPI/APIInterface.java
@@ -0,0 +1,35 @@
+package com.notmiyouji.newsapp.NewsAPI;
+
+import com.notmiyouji.newsapp.NewsAPI.NewsAPIModels.Category.NewsCategory;
+import com.notmiyouji.newsapp.NewsAPI.NewsAPIModels.News;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface APIInterface {
+
+    @GET("top-headlines")
+    Call<News> getLatestNews(
+
+            @Query("country") String country ,
+            @Query("apiKey") String apiKey
+
+    );
+    @GET("top-headlines")
+    Call<NewsCategory> getNewsCategory(
+            @Query("country") String country ,
+            @Query("category") String category,
+            @Query("apiKey") String apiKey
+
+    );
+    @GET("everything")
+    Call<News> getNewsSearch(
+
+            @Query("q") String keyword,
+            @Query("language") String language,
+            @Query("sortBy") String sortBy,
+            @Query("apiKey") String apiKey
+
+    );
+}
---
+++ app/src/main/java/com/notmiyouji/newsapp/global/recycleviewadapter/newsapi/NewsAdapterVertical.java
@@ -0,0 +1,79 @@
+package com.notmiyouji.newsapp.global.recycleviewadapter.newsapi;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions;
+import com.bumptech.glide.request.RequestOptions;
+import com.notmiyouji.newsapp.NewsAPI.NewsAPIModels.Category.ArticleCategory;
+import com.notmiyouji.newsapp.NewsAPI.Utils;
+import com.notmiyouji.newsapp.R;
+
+import java.util.List;
+
+public class NewsAdapterVertical extends RecyclerView.Adapter<NewsAdapterVertical.MyViewHolder> {
+
+
+    AppCompatActivity activity;
+    List<ArticleCategory> articleCategory;
+
+    public NewsAdapterVertical(List<ArticleCategory> articleCategory, AppCompatActivity activity) {
+        this.activity = activity;
+        this.articleCategory = articleCategory;
+    }
+
+    @NonNull
+    @Override
+    public NewsAdapterVertical.MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(activity).inflate(R.layout.news_items_vertical, parent, false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull NewsAdapterVertical.MyViewHolder holders, int position) {
+        ArticleCategory model = articleCategory.get(position);
+        RequestOptions requestOptions = new RequestOptions();
+        Glide.with(activity)
+                .load(model.getUrlToImage())
+                .apply(requestOptions)
+                .transition(DrawableTransitionOptions.withCrossFade())
+                .into(holders.imageView);
+
+        holders.title.setText(model.getTitle());
+        holders.source.setText(model.getSource().getName());
+        holders.time.setText(" \u2022 " + Utils.DateToTimeFormat(model.getPublishedAt()));
+    }
+
+    @Override
+    public int getItemCount() {
+        return articleCategory.size();
+    }
+
+    public interface ItemClickListener {
+        void onClick(View view, int position, boolean isLongClick);
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+
+        TextView title, source, time;
+        ImageView imageView;
+        private ItemClickListener itemClickListener;
+
+        public MyViewHolder(View itemView) {
+
+            super(itemView);
+            title = itemView.findViewById(R.id.txtTitle);
+            source = itemView.findViewById(R.id.txtSource);
+            time = itemView.findViewById(R.id.txtPubDate);
+            imageView = itemView.findViewById(R.id.imgNews);
+        }
+    }
+}
